328d27051fab4ec2196932477ebd9fd0c3fb7578
Koki Fushimi <paalon1936@gmail.com>
2018-05-18 00:40:28 +0900

    Add tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,11 @@ else
     using Test
 end
 
-# test
+# tests
+
 @test squarewave(0) == 1.0
 @test squarewave(1π) == 1.0
 @test squarewave(2π) == -1.0
+@test squarewave(0.4) == 1.0
+@test sawtoothwave(0.4) == 0.1273239544735163
+@test trianglewave(0.4) == 0.2546479089470326
