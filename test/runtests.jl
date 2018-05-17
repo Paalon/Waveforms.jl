a4f411ba5806e6ec13678d005b91fe659b8fc42e
Koki Fushimi <paalon1936@gmail.com>
2018-05-18 04:28:47 +0900

    Add tests for 1-periodic functions.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,16 @@ end
 
 # tests
 
-@test squarewave(0) == 1.0
-@test squarewave(1π) == 1.0
-@test squarewave(2π) == -1.0
 @test squarewave(0.4) == 1.0
-@test sawtoothwave(0.4) == 0.1273239544735163
+@test squarewave1(0.4) == 1.0
+@test sawtoothwave(0.4) == 0.12732395447351627
+@test sawtoothwave1(0.4) == 0.8
 @test trianglewave(0.4) == 0.2546479089470326
+@test trianglewave1(0.4) == 0.3999999999999999
+
+@test squarewave(3π/7) == 1.0
+@test squarewave1(3π/7) == 1.0
+@test sawtoothwave(3π/7) == 0.42857142857142855
+@test sawtoothwave1(3π/7) == 0.6927937030769655
+@test trianglewave(3π/7) == 0.8571428571428572
+@test trianglewave1(3π/7) == 0.6144125938460689
