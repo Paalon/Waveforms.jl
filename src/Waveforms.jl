c7c36c1fcfdf23d11bf62cae9a9b65418548d142
Koki Fushimi <paalon1936@gmail.com>
2018-05-18 04:18:32 +0900

    Add 1-periodic functions.

--- src/Waveforms.jl
+++ src/Waveforms.jl
@@ -1,4 +1,4 @@
-# __precompile__()
+__precompile__()
 
 module Waveforms
 
@@ -15,6 +15,14 @@ squarewave(x::Real) = ifelse(mod2pi(x) < π, 1.0, -1.0)
 squarewave(x::Real, θ::Real) = ifelse(mod2pi(x) < 2π*θ, 1.0, -1.0)
 
 """
+    squarewave1(x)
+
+1-periodic square wave function, min = -1, max = +1.
+
+"""
+squarewave1(x::Real) = ifelse(mod(x, 1) < 1/2, 1.0, -1.0)
+
+"""
     trianglewave(x)
 
 2π-periodic triangle wave function, min = -1, max = +1.
@@ -26,11 +34,30 @@ function trianglewave(x::Real)
 end
 
 """
+    trianglewave1(x)
+
+1-periodic triangle wave function, min = -1, max = +1.
+
+"""
+function trianglewave1(x::Real)
+    modx = mod(x + 1/4, 1.0)
+    ifelse(modx < 1/2, 4modx - 1, -4modx + 3)
+end
+
+"""
     sawtoothwave(x)
 
 2π-periodic sawtooth wave function, min = -1, max = +1.
 
 """
-sawtoothwave(x::Real) = mod2pi(x-π)/π - 1
+sawtoothwave(x::Real) = rem2pi(x, RoundNearest)/π
+
+"""
+    sawtoothwave1(x)
+
+1-periodic sawtooth wave function, min = -1, max = +1.
+
+"""
+sawtoothwave1(x::Real) = rem(x, 1.0, RoundNearest)*2
 
 end # module
