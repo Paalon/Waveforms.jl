9833cf3f0009b35ab8af6676ef671195f3761304
Koki Fushimi <paalon1936@gmail.com>
2018-05-18 04:45:22 +0900

    Update documents.

--- src/Waveforms.jl
+++ src/Waveforms.jl
@@ -13,26 +13,22 @@ export
 """
     squarewave(x)
 
-2π-periodic square wave function, min = -1, max = +1.
-
+Compute 2π-periodic square wave of `x`. min = -1, max = +1.
 """
-
 squarewave(x::Real) = ifelse(mod2pi(x) < π, 1.0, -1.0)
 squarewave(x::Real, θ::Real) = ifelse(mod2pi(x) < 2π*θ, 1.0, -1.0)
 
 """
     squarewave1(x)
 
-1-periodic square wave function, min = -1, max = +1.
-
+Compute 1-periodic square wave of `x`. min = -1, max = +1.
 """
 squarewave1(x::Real) = ifelse(mod(x, 1) < 1/2, 1.0, -1.0)
 
 """
     trianglewave(x)
 
-2π-periodic triangle wave function, min = -1, max = +1.
-
+Compute 2π-periodic triangle wave of `x`. min = -1, max = +1.
 """
 function trianglewave(x::Real)
     modx = mod2pi(x + π/2)
@@ -42,8 +38,7 @@ end
 """
     trianglewave1(x)
 
-1-periodic triangle wave function, min = -1, max = +1.
-
+Compute 1-periodic triangle wave of `x`. min = -1, max = +1.
 """
 function trianglewave1(x::Real)
     modx = mod(x + 1/4, 1.0)
@@ -53,16 +48,14 @@ end
 """
     sawtoothwave(x)
 
-2π-periodic sawtooth wave function, min = -1, max = +1.
-
+Compute 2π-periodic sawtooth wave of `x`. min = -1, max = +1.
 """
 sawtoothwave(x::Real) = rem2pi(x, RoundNearest)/π
 
 """
     sawtoothwave1(x)
 
-1-periodic sawtooth wave function, min = -1, max = +1.
-
+Compute 1-periodic sawtooth wave of `x`. min = -1, max = +1.
 """
 sawtoothwave1(x::Real) = rem(x, 1.0, RoundNearest)*2
 
