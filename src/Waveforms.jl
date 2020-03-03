ba0151deb2252e229519e57fed48e0e665351d07
Koki Fushimi <paalon1936@gmail.com>
2020-03-03 18:37:01 +0900

    Update documents.

--- src/Waveforms.jl
+++ src/Waveforms.jl
@@ -8,30 +8,29 @@ export
     trianglewave,
     trianglewave1
 
-"""
+@doc raw"""
     squarewave(x)
 
-Compute 2π-periodic square wave of `x`. min = -1, max = +1.
+Compute ``2\pi``-periodic square wave of `x` with a peak amplitude ``1``.
 """
 squarewave(x::Real) = ifelse(mod2pi(x) < π, 1.0, -1.0)
 
-"""
+@doc raw"""
     squarewave(x, θ)
 
-Compute 2π-periodic square wave of `x`. min = -1, max = +1.
-squarewave(x) = +1.0 for 0   < mod(x, 2π) < 2π0,
-squarewave(x) = -1.0 for 2π0 < mod(x, 2π) < 2π.
+Compute ``2\pi``-periodic square wave of `x` with a duty cycle `θ`
+and a peak amplitude ``1``.
 """
 squarewave(x::Real, θ::Real) = ifelse(mod2pi(x) < 2π*θ, 1.0, -1.0)
 
-"""
+@doc raw"""
     squarewave1(x)
 
-Compute 1-periodic square wave of `x`. min = -1, max = +1.
+Compute ``1``-periodic square wave of `x` with a peak amplitude ``1``.
 """
 squarewave1(x::Real) = ifelse(mod(x, 1) < 1/2, 1.0, -1.0)
 
-"""
+@doc raw"""
     squarewave1(x, θ)
 
 Compute ``1``-periodic square wave of `x` with a duty cycle `θ`
@@ -42,37 +41,37 @@ function squarewave1(x::Real, θ::Real)
     ifelse(mod(x, 1) < θ, 1.0, -1.0)
 end
 
-"""
+@doc raw"""
     trianglewave(x)
 
-Compute 2π-periodic triangle wave of `x`. min = -1, max = +1.
+Compute ``2\pi``-periodic triangle wave of `x` with a peak amplitude ``1``.
 """
 function trianglewave(x::Real)
     modx = mod2pi(x + π/2)
     ifelse(modx < π, 2modx/π - 1, -2modx/π + 3)
 end
 
-"""
+@doc raw"""
     trianglewave1(x)
 
-Compute 1-periodic triangle wave of `x`. min = -1, max = +1.
+Compute ``1``-periodic triangle wave of `x` with a peak amplitude ``1``.
 """
 function trianglewave1(x::Real)
     modx = mod(x + 1/4, 1.0)
     ifelse(modx < 1/2, 4modx - 1, -4modx + 3)
 end
 
-"""
+@doc raw"""
     sawtoothwave(x)
 
-Compute 2π-periodic sawtooth wave of `x`. min = -1, max = +1.
+Compute ``2\pi``-periodic sawtooth wave of `x` with a peak amplitude ``1``.
 """
 sawtoothwave(x::Real) = rem2pi(x, RoundNearest)/π
 
-"""
+@doc raw"""
     sawtoothwave1(x)
 
-Compute 1-periodic sawtooth wave of `x`. min = -1, max = +1.
+Compute ``1``-periodic sawtooth wave of `x` with a peak amplitude ``1``.
 """
 sawtoothwave1(x::Real) = rem(x, 1.0, RoundNearest)*2
 
